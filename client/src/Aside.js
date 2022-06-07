a05a483a4c8cfc04522fe7b9b85101ab70289588
monde1999 <monchingdesierto49@gmail.com>
2022-06-08 01:45:35 +0800

    applied bootstrap to nav aside and dashboard product

---
+++ client/src/Aside.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './bootstrap-4.0.0-dist/css/bootstrap.min.css'
+
+function Aside() {
+    return (
+        <aside>
+            <br/>
+            <br/>
+            <button class="btn btn-primary btn-lg btn-block">Customer</button>
+            <br/>
+            <button class="btn btn-lg btn-block">Product</button>
+        </aside>
+    );
+}
+
+export default Aside;
--- client/src/DashboardProduct.js
+++ client/src/DashboardProduct.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import './DashboardProduct.css'
+import Aside from "./Aside";
 import './bootstrap-4.0.0-dist/css/bootstrap.min.css'
 
 function DashboardProduct () {
@@ -53,36 +53,42 @@ function DashboardProduct () {
     // }, []);
 
     return (
-        <div>
-            <h2>Dashboard Products</h2>
-            
-            <table class="table table-bordered table-hover">
-                <thead class="bg-info text-white">
-                    <tr>
-                        <th>ID no.</th>
-                        <th>Datestamp</th>
-                        <th>Category</th>
-                        <th>Brand</th>
-                        <th>Name</th>
-                        <th>Size</th>
-                        <th>Price</th>
-                        <th>Stocks</th>
-                        <th>Photos</th>
-                        <th>Edit</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { data.map((d) => (
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <Aside />
+                </div>
+                <div class="col-10">
+                <h2>Dashboard Products</h2>
+                <table class="table table-bordered table-hover">
+                    <thead class="bg-info text-white">
                         <tr>
-                            { Object.entries(d).map(([key,value]) =>
-                                <td>{ key!=='edit' ? value : <button class="btn btn-warning">Edit</button> }</td>
-                            ) }
+                            <th>ID no.</th>
+                            <th>Datestamp</th>
+                            <th>Category</th>
+                            <th>Brand</th>
+                            <th>Name</th>
+                            <th>Size</th>
+                            <th>Price</th>
+                            <th>Stocks</th>
+                            <th>Photos</th>
+                            <th>Edit</th>
                         </tr>
-                    )) }
-                </tbody>
-            </table> 
+                    </thead>
+                    <tbody>
+                        { data.map((d) => (
+                            <tr>
+                                { Object.entries(d).map(([key,value]) =>
+                                    <td>{ key!=='edit' ? value : <button class="btn btn-warning">Edit</button> }</td>
+                                ) }
+                            </tr>
+                        )) }
+                    </tbody>
+                </table> 
 
-            <button class="btn btn-primary">Add Product</button>
+                <button class="btn btn-primary">Add Product</button>
+                </div>
+            </div>
         </div>
     );
 }
