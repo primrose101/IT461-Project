7cc9b15e2ed0bdc371b394ebb5c94588587527f9
monde1999 <monchingdesierto49@gmail.com>
2022-06-08 22:08:15 +0800

    server-side: registered models in admins

--- server/MySample/MySample/dashboard/admin.py
+++ server/MySample/MySample/dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Customer, OrderItems
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(OrderItems)
